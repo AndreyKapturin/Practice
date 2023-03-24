0732bf3fcc845c176c76fa7f11b98ffd39cb41d7
Andrey Kapturin <andruxa21097@gmail.com>
2023-03-24 17:28:41 +0700

    Парсинг покемонов

--- script/script.js
+++ script/script.js
@@ -1,3 +1,20 @@
-console.log("hello world");
+const cards = document.querySelector(".cards")
 
-test
+const link = "https://fakerapi.it/api/v1/images?_quantity=12&_type=pokemon&_height=300"
+
+fetch(link)
+    .then(result => {
+        return result.json()
+    })
+    .then(result => {
+        result.data.forEach(pok => {
+            const div = document.createElement("div");
+            const img = document.createElement("img");
+            div.classList.add("card");
+            img.src = pok.url;
+            img.height = 150;
+            img.width = 200;
+            cards.append(div);
+            cards.lastElementChild.append(img)
+    })     
+    })
